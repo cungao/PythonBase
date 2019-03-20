30f53f5a15181cdc3dbee1f129a07d9946801e8f
cun.gao <cun>
2019-03-20 16:54:25 +0800

    创建了一个新的package
    Signed-off-by: cun.gao <cun>

--- python_基础/随机数.py
+++ python_基础/随机数.py
@@ -4,5 +4,5 @@ print(random.uniform(1,10))
 #生成[a,b]指定范围的随机数-整数
 print(random.randint(1,10))
 #随机选取n个字符
-print(random.sample('gaocundawang',2))
-print(random.sample('gaocundawang',2))
+print(random.sample('gaocundawang',2111))
+print(random.sample('gaocundawang',2111))
